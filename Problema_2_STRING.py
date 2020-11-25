24f660f1e5a2449ee5fa5b5b8f2e487b4d0493d6
mariaciornii <70654766+mariaciornii@users.noreply.github.com>
2020-11-25 12:33:48 +0200

    Update Problema_2_STRING.py

--- Problema_2_STRING.py
+++ Problema_2_STRING.py
@@ -1,14 +1,14 @@
-sir=str(input('Sir: '))
-maj=0
-cif=0
-cr_sp=0
-for i in sir:
+s=str(input('Sirul: '))
+m=0
+c=0
+csp=0
+for i in s:
     if ord(i) in range(65,91):
-        maj+=1
+        m+=1
     if ord(i) in range(48,58):
-        cif+=1
+        c+=1
     if ord(i) in range(32,48) or ord(i) in range(58,65) or ord(i) in range(91,97) or ord(i) in range(123,127):
-        cr_sp+=1
-print(f'a) Numarul de majuscule in sir: {maj}')
-print(f'b) Numarul de cifre in sir: {cif}')
-print(f'c) Numarul de caractere speciale in sir: {cr_sp}')
+        csp+=1
+print('Majuscule in sir:', m)
+print('Cifre in sir:', c)
+print('Caractere speciale in sir:', csp)
